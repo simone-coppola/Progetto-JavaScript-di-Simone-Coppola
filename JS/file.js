5d3a886001d07d30ec490086dcce4a346423fe7d
Mognek <simopp0301@gmail.com>
2025-09-13 17:17:13 +0200

    sistemato tutti i file correttamente

---
+++ JS/file.js
@@ -0,0 +1,34 @@
+        const body = document.querySelector('body');
+        
+        const container = document.querySelector('.container');
+
+        const title = document.createElement('h1');
+        title.textContent = 'Counter';
+        container.appendChild(title);
+
+        const valueCounter = document.createElement('div');
+        valueCounter.textContent = '0';
+        valueCounter.classList.add('valueCounter');
+        container.appendChild(valueCounter);
+
+        const decrementButton = document.createElement('button');
+        decrementButton.textContent = '-';
+        decrementButton.classList.add('decrementButton');
+        container.appendChild(decrementButton);
+
+        const incrementButton = document.createElement('button');
+        incrementButton.textContent = '+';
+        incrementButton.classList.add('incrementButton');
+        container.appendChild(incrementButton);
+
+        let counter = 1;
+
+        decrementButton.addEventListener('click', () => {
+            counter--;
+            valueCounter.textContent = counter;
+        });
+
+        incrementButton.addEventListener('click', () => {
+            counter++;
+            valueCounter.textContent = counter;
+        });
