47def83d8cec1d268bf5ecdc6666d11471cc7844
Mognek <simopp0301@gmail.com>
2025-09-13 17:21:42 +0200

    modificato correttamente i link

--- JS/file.js
+++ JS/file.js
@@ -1,34 +1,32 @@
-        const body = document.querySelector('body');
-        
-        const container = document.querySelector('.container');
+const container = document.querySelector('.container');
 
-        const title = document.createElement('h1');
-        title.textContent = 'Counter';
-        container.appendChild(title);
+const title = document.createElement('h1');
+title.textContent = 'Counter';
+container.appendChild(title);
 
-        const valueCounter = document.createElement('div');
-        valueCounter.textContent = '0';
-        valueCounter.classList.add('valueCounter');
-        container.appendChild(valueCounter);
+const valueCounter = document.createElement('div');
+valueCounter.textContent = '0';
+valueCounter.classList.add('valueCounter');
+container.appendChild(valueCounter);
 
-        const decrementButton = document.createElement('button');
-        decrementButton.textContent = '-';
-        decrementButton.classList.add('decrementButton');
-        container.appendChild(decrementButton);
+const decrementButton = document.createElement('button');
+decrementButton.textContent = '-';
+decrementButton.classList.add('decrementButton');
+container.appendChild(decrementButton);
 
-        const incrementButton = document.createElement('button');
-        incrementButton.textContent = '+';
-        incrementButton.classList.add('incrementButton');
-        container.appendChild(incrementButton);
+const incrementButton = document.createElement('button');
+incrementButton.textContent = '+';
+incrementButton.classList.add('incrementButton');
+container.appendChild(incrementButton);
 
-        let counter = 1;
+let counter = 1;
 
-        decrementButton.addEventListener('click', () => {
-            counter--;
-            valueCounter.textContent = counter;
-        });
+decrementButton.addEventListener('click', () => {
+    counter--;
+    valueCounter.textContent = counter;
+});
 
-        incrementButton.addEventListener('click', () => {
-            counter++;
-            valueCounter.textContent = counter;
-        });
+incrementButton.addEventListener('click', () => {
+    counter++;
+    valueCounter.textContent = counter;
+});
